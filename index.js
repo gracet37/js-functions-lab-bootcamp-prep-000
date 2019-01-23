a29ae2f02b205ec734eea09a5a48df7f5a476651
Grace Tan <gracetan37@gmail.com>
2019-01-23 08:21:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 }
 happyHolidaysTo('you')
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}`
+}
+function holidayCountdown(holiday, days) {
+  return `It's ${days} until ${holiday}!` 
+}
